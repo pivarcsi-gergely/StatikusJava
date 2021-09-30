e7ab5512a03c673d30c694caa5f7ecd2ff1f7b3e
Pivarcsi Gergely Mate <gergely.pivarcsi@gmail.com>
2021-09-30 11:37:50 +0200

    Initial commit

---
+++ src/hu/petrik/Main.java
@@ -0,0 +1,29 @@
+package hu.petrik;
+
+public class Main {
+
+    private int simaAdattag = 0;
+    private static int statikusAdattag = 0;
+
+    public static void main(String[] args) {
+	/*
+	Experimental
+	// Ez nem kell (állítólag:)
+
+        Main Legfobb = new Main();
+
+        //Ezt pedig nem lehet létrehozni
+        //Math matek = new Matek();
+        System.out.println(statikusAdattag);
+        System.out.println(simaAdattag);-+
+        System.out.println(Legfobb.simaAdattag);*/
+    }
+/*
+    public void nemStatikusMetodus(){
+        System.out.println(this.simaAdattag);
+        // this kulcsszó objektumra utal
+        // Statikus adattagot el lehet érni objektumpéldány változójaként
+        System.out.println(this.statikusAdattag);
+    }
+ */
+}
---
+++ src/hu/petrik/Veletlen.java
@@ -0,0 +1,36 @@
+package hu.petrik;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public final class Veletlen {
+
+    private static Random random = new Random();
+    private static List<String> egyesuletek = feltolt("egyesulet.txt");
+    private static List<String> esemenyTipusok = feltolt("esemenytipus.txt");
+    private static List<String> ferfiKerNevek = feltolt("ferfikernev.txt");
+    private static List<String> noiKerNevek = feltolt("noikernev.txt");
+    private static List<String> sportagok = feltolt("sportag.txt");
+    private static List<String> VezNevek = feltolt("veznev.txt");
+
+    private Veletlen(){}
+
+    private static List<String> feltolt(String fajlNev){
+        List<String> fajlSorai = new ArrayList<>();
+        try{
+            fajlSorai = Files.readAllLines(Paths.get(fajlNev));
+        }
+        catch (IOException e){
+            e.getMessage();
+        }
+        return fajlSorai;
+    }
+
+    private static int velEgesz(int max, int min){
+        return random.nextInt(10)+1;
+    }
+}
